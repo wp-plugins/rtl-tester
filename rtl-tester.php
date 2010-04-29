8bc8a8de8a8e41b801091acd3ed21a1c4117e1c1
yoavf <yoavf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-29 17:54:13 +0000

    update plugin url
    git-svn-id: https://plugins.svn.wordpress.org/rtl-tester/trunk@235457 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rtl-tester.php
+++ rtl-tester.php
@@ -5,7 +5,7 @@
  */
 /*
 Plugin Name: RTL Tester
-Plugin URI: http://blog.yoavfarhi.com/rtl_tester
+Plugin URI: http://wordpress.org/extend/plugins/rtl-tester/
 Description: This plugin can be used to test WordPress themes and plugins with Right To Left (RTL) text direction. Simply activate this plugin to enable RTL and deactivate to return to normal operation.
 Author: <a href="http://blog.yoavfarhi.com">Yoav Farhi</a>, <a href="http://automattic.com">Automattic</a>
 Version: 0.1
