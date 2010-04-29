601197b0835ebf9231bc5155b61040a969ecfae4
yoavf <yoavf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-29 17:34:25 +0000

    checking in 0.1
    git-svn-id: https://plugins.svn.wordpress.org/rtl-tester/trunk@235448 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Plugin Name ===
+Contributors: automattic, yoavf
+Tags: RTL, testing
+Requires at least: 2.8
+Tested up to: trunk
+Stable tag: 2.9
+Tested up to: 3.0
+
+Test your themes and plugins in RTL mode.
+
+== Description ==
+
+This plugin can be used to test WordPress themes and plugins with Right To Left (RTL) text direction. Simply activate this plugin to enable RTL and deactivate to return to normal operation.
+
+== Installation ==
+
+Upload, Activate
+
+== Changelog ==
+
+= 0.1 =
+Initial version.
---
+++ rtl-tester.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package RTL_Tester
+ * @version 0.1
+ */
+/*
+Plugin Name: RTL Tester
+Plugin URI: http://blog.yoavfarhi.com/rtl_tester
+Description: This plugin can be used to test WordPress themes and plugins with Right To Left (RTL) text direction. Simply activate this plugin to enable RTL and deactivate to return to normal operation.
+Author: <a href="http://blog.yoavfarhi.com">Yoav Farhi</a>, <a href="http://automattic.com">Automattic</a>
+Version: 0.1
+*/
+
+function force_text_direction_rtl() {
+	global $wp_locale;
+	if ( isset( $wp_locale ) )
+		$wp_locale->text_direction = 'rtl';
+}
+
+add_action( 'init', 'force_text_direction_rtl' );
+
+?>
